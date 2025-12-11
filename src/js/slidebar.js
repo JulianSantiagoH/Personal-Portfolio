282ece94ca74a802e8097380cbc81d73b24ddce7
Julian Herrera <juliansantiago.rubianoherrera@gmail.com>
2025-12-11 15:35:43 -0500

    bg dark added when the slidebar is open

--- src/js/slidebar.js
+++ src/js/slidebar.js
@@ -1,23 +1,21 @@
 const showSlidebarID = document.getElementById("showSlidebar");
 const hiddenSlidebarID = document.getElementById("hiddenSlidebar");
 const slidebar = document.getElementById('slidebar')
+const bgDark = document.getElementById('bgDark')
 
 const getClassnameSlidebar = slidebar.classList.value;
+const getClassnameBgDark = bgDark.classList.value
 
 let classNameSlidebarOpened = getClassnameSlidebar.split(" ");
-
-console.log(classNameSlidebarOpened);
+let classNameBgDark = getClassnameBgDark.split(' ')
 
 showSlidebarID.addEventListener("click", () => {
-  if (classNameSlidebarOpened.includes("hidden-slidebar")) {
+  if (classNameSlidebarOpened.includes("hidden-slidebar") && classNameBgDark.includes("hidden-slidebar")) {
     slidebar.classList.remove("hidden-slidebar");
-    console.log("si esta");
-  } else {
-    slidebar.classList.add("hidden-slidebar");
-    console.log("no esta");
-  }
-});
+    bgDark.classList.remove("hidden-slidebar")
+  }});
 
 hiddenSlidebarID.addEventListener('click',()=>{
     slidebar.classList.add("hidden-slidebar");
+    bgDark.classList.add("hidden-slidebar");
 })
